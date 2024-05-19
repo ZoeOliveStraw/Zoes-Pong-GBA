14ec6b9def232fff978922911e5bb17c1eee15c3
Zoe Straw <zoe.olive.straw@gmail.com>
2024-05-19 09:54:50 -0400

    forgot a file lol

--- source/main_menu.c
+++ source/main_menu.c
@@ -4,11 +4,6 @@
 
 void MainMenu()
 {
-    for(int i = 0; i < 64; i++)
-    {
-        if(*score + i) DrawPixel(20 + i, 20, 0x7FFF);
-    }
-
     struct input* Input = (struct input*)malloc(sizeof(struct rect));
     PrintCharacter(&score[0], 5, 5);
 
